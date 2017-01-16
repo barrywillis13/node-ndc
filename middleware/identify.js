682d308cea1c53694cf1edebd3028b137d98a3e0
barrywillis13 <barrywillis13@gmail.com>
2017-01-16 15:02:57 +0000

    added indexOf for identify function

--- middleware/identify.js
+++ middleware/identify.js
@@ -1,30 +1,24 @@
 
 var identify = (req, res, next) => {
   var reqString = req.body.toString();
-  var index = 0
 
-  if (reqString.startsWith('<?xml version="1.0" encoding="UTF-8"?>')){
-    index = 40
-  } else if (reqString.startsWith('<?xml version="1.0" encoding="UTF-16"?>')){
-    index = 41
-  }
   if(reqString.startsWith('<AirShoppingRQ', reqString.indexOf('<AirShoppingRQ'))){
       req.id = 'shop';
-  } else if(reqString.startsWith('<AirDocCancelRQ', index)){
+  } else if(reqString.startsWith('<AirDocCancelRQ', reqString.indexOf('<AirDocCancelRQ'))){
       req.id = 'cancelTicket';
-  } else if(reqString.startsWith('<AirDocIssueRQ', index)){
+  } else if(reqString.startsWith('<AirDocIssueRQ', reqString.indexOf('<AirDocIssueRQ'))){
       req.id = 'ticket';
   } else if(reqString.startsWith('<FareRulesRQ', reqString.indexOf('<FareRulesRQ'))){
       req.id = 'rules';
-  } else if(reqString.startsWith('<FlightPriceRQ', index)){
+  } else if(reqString.startsWith('<FlightPriceRQ', reqString.indexOf('<FlightPriceRQ'))){
       req.id = 'airfare';
-  } else if(reqString.startsWith('<OrderCancelRQ', index)){
+  } else if(reqString.startsWith('<OrderCancelRQ', reqString.indexOf('<OrderCancelRQ'))){
       req.id = 'cancel';
-  } else if(reqString.startsWith('<OrderChangeRQ', index)){
+  } else if(reqString.startsWith('<OrderChangeRQ', reqString.indexOf('<OrderChangeRQ'))){
       req.id = 'modify';
-  } else if(reqString.startsWith('<OrderViewRQ', index)){
+  } else if(reqString.startsWith('<OrderViewRQ', reqString.indexOf('<OrderViewRQ'))){
       req.id = 'read';
-  } else if(reqString.startsWith('<SeatAvailabilityRQ', index)){
+  } else if(reqString.startsWith('<SeatAvailabilityRQ', reqString.indexOf('<SeatAvailabilityRQ'))){
       req.id = 'seatmap';
   }
   next();
