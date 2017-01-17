c98b7b4429487a6c46e9d4afe301988c7c42d3a0
barrywillis13 <barrywillis13@gmail.com>
2017-01-17 11:36:20 +0000

    GET message added

--- server/server.js
+++ server/server.js
@@ -21,18 +21,13 @@ app.timeout = 10000;
 ////////////////////////////////////////////////////////////////////////////////
 
 app.post('/', (req, res) => {
-  //  setTimeout(() => {
-  //    if(req.body){
-  //      res.status(504).send('Request timed out')
-  //    }
-  //  }, 10000)
    execute(req.body.toString(), req.id.toString(), (result) => {
       return res.send(result).end()
    })
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello Michael...')
+  res.send('This server does not support GET requests, so it doesnt, so it is!')
 })
 
 ////////////////////////////////////////////////////////////////////////////////
